f05fb0296cfc29e85506123831c68bc2976eed74
paolovelarde <paolo.velarde@mail.utoronto.ca>
2019-09-19 17:16:07 -0400

    Updates

---
+++ precache-manifest.1f67cffafb4a4e3b95b636f1977e3493.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3256ce67e9dba9f19a31e02f50e142e4",
+    "url": "/reactportfolio/index.html"
+  },
+  {
+    "revision": "ddc9dbce7cf824cc251c",
+    "url": "/reactportfolio/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d9c6a2ca9edc818a2c47",
+    "url": "/reactportfolio/static/js/2.71bc8469.chunk.js"
+  },
+  {
+    "revision": "ddc9dbce7cf824cc251c",
+    "url": "/reactportfolio/static/js/main.2e9be487.chunk.js"
+  },
+  {
+    "revision": "545faefdce172d01a44f",
+    "url": "/reactportfolio/static/js/runtime-main.6a62ed75.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/reactportfolio/static/media/logo.25bf045c.svg"
+  }
+]);
