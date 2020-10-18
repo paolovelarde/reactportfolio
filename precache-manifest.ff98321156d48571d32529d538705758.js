2754dc99a217c2a24d966c0993e946dc9462e5e6
unknown <p.velarde@live.com>
2020-10-18 19:03:41 -0400

    Updates

---
+++ precache-manifest.ff98321156d48571d32529d538705758.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6753341c0e595d824ecbc4431e910ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6eb9904bd09110c462a4",
+    "url": "/static/css/main.92444dca.chunk.css"
+  },
+  {
+    "revision": "1ab97aeba47a3da5c859",
+    "url": "/static/js/2.3d177074.chunk.js"
+  },
+  {
+    "revision": "6eb9904bd09110c462a4",
+    "url": "/static/js/main.d4b69583.chunk.js"
+  },
+  {
+    "revision": "7d8a8798807489d771e4",
+    "url": "/static/js/runtime-main.87b78075.js"
+  },
+  {
+    "revision": "d71c7287a447bc50c78f444cc30ad5c8",
+    "url": "/static/media/FI.d71c7287.png"
+  },
+  {
+    "revision": "410b72ff287a490ef0feed309421748d",
+    "url": "/static/media/KP.410b72ff.png"
+  },
+  {
+    "revision": "71aa9b9d4b810710e56bb8def12e2839",
+    "url": "/static/media/PDBlack.71aa9b9d.ttf"
+  },
+  {
+    "revision": "54f612f47583d50e862f88908f285b45",
+    "url": "/static/media/PDBold.54f612f4.ttf"
+  },
+  {
+    "revision": "2ae16087c60e8bc28d2f9547c74349d1",
+    "url": "/static/media/PDReg.2ae16087.ttf"
+  },
+  {
+    "revision": "325e3eddf37b7124db52a483004a1f54",
+    "url": "/static/media/SPOT.325e3edd.png"
+  },
+  {
+    "revision": "9b62dc86f936227b3f7b367bd0b6c05e",
+    "url": "/static/media/TwBold.9b62dc86.TTF"
+  },
+  {
+    "revision": "d3ab0a606fd2ffbe8f8fa869f382986e",
+    "url": "/static/media/TwItalic.d3ab0a60.TTF"
+  },
+  {
+    "revision": "2c8622286a44972d1ba4eff9ef890a7a",
+    "url": "/static/media/TwReg.2c862228.otf"
+  },
+  {
+    "revision": "1b38fce0876226b981c42efa6269eb1d",
+    "url": "/static/media/bgLaptop.1b38fce0.png"
+  },
+  {
+    "revision": "dba91dc5339e68ad23242b86031471f4",
+    "url": "/static/media/chevronDown.dba91dc5.png"
+  },
+  {
+    "revision": "950055f9b805192dcdeedc478bff6705",
+    "url": "/static/media/chevronDownWht.950055f9.png"
+  },
+  {
+    "revision": "1c6080076515f85472e3dda905dc34f7",
+    "url": "/static/media/chevronUpWht.1c608007.png"
+  },
+  {
+    "revision": "1e0e1c69cfeb17c982b2840171e684bd",
+    "url": "/static/media/csec-proj.1e0e1c69.png"
+  },
+  {
+    "revision": "439de75bae8f62cbbf2aa5ff5b7486c8",
+    "url": "/static/media/me.439de75b.png"
+  },
+  {
+    "revision": "a2e41c6d759f272a01a5a1c5175b77f9",
+    "url": "/static/media/resume.a2e41c6d.pdf"
+  },
+  {
+    "revision": "e893f7f78381ae3d090f313d0fb4df7e",
+    "url": "/static/media/yellow.e893f7f7.png"
+  }
+]);
