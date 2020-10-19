c2dcbb3eba7a23af67da033faaf8e6aed5b632f7
Paolo Velarde <39651860+paolovelarde@users.noreply.github.com>
2020-10-18 21:43:49 -0400

    Add files via upload

---
+++ precache-manifest.70e588a51a7e71119e66a737a652008b.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5df4de185252baebf28dc3e0ba5f0d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "452ebf3f0527166bc12c",
+    "url": "/static/css/main.d816f319.chunk.css"
+  },
+  {
+    "revision": "ca026feff5007e6ccc1e",
+    "url": "/static/js/2.c1763a6c.chunk.js"
+  },
+  {
+    "revision": "452ebf3f0527166bc12c",
+    "url": "/static/js/main.5b6c94af.chunk.js"
+  },
+  {
+    "revision": "7d8a8798807489d771e4",
+    "url": "/static/js/runtime-main.87b78075.js"
+  },
+  {
+    "revision": "13448818db042f00c29aa075f7a1ec4f",
+    "url": "/static/media/FI.13448818.png"
+  },
+  {
+    "revision": "7cb53fad6fd81c55ec62ba1c06352fd5",
+    "url": "/static/media/KP.7cb53fad.png"
+  },
+  {
+    "revision": "71aa9b9d4b810710e56bb8def12e2839",
+    "url": "/static/media/PDBlack.71aa9b9d.ttf"
+  },
+  {
+    "revision": "54f612f47583d50e862f88908f285b45",
+    "url": "/static/media/PDBold.54f612f4.ttf"
+  },
+  {
+    "revision": "2ae16087c60e8bc28d2f9547c74349d1",
+    "url": "/static/media/PDReg.2ae16087.ttf"
+  },
+  {
+    "revision": "d2ac51ec6b7180b62ef247d26afed4bd",
+    "url": "/static/media/SPOT.d2ac51ec.png"
+  },
+  {
+    "revision": "9b62dc86f936227b3f7b367bd0b6c05e",
+    "url": "/static/media/TwBold.9b62dc86.TTF"
+  },
+  {
+    "revision": "d3ab0a606fd2ffbe8f8fa869f382986e",
+    "url": "/static/media/TwItalic.d3ab0a60.TTF"
+  },
+  {
+    "revision": "2c8622286a44972d1ba4eff9ef890a7a",
+    "url": "/static/media/TwReg.2c862228.otf"
+  },
+  {
+    "revision": "1b38fce0876226b981c42efa6269eb1d",
+    "url": "/static/media/bgLaptop.1b38fce0.png"
+  },
+  {
+    "revision": "b009b5a984138ed4d7aa763f57edf2e7",
+    "url": "/static/media/bgLaptopWhite.b009b5a9.png"
+  },
+  {
+    "revision": "dba91dc5339e68ad23242b86031471f4",
+    "url": "/static/media/chevronDown.dba91dc5.png"
+  },
+  {
+    "revision": "950055f9b805192dcdeedc478bff6705",
+    "url": "/static/media/chevronDownWht.950055f9.png"
+  },
+  {
+    "revision": "1c6080076515f85472e3dda905dc34f7",
+    "url": "/static/media/chevronUpWht.1c608007.png"
+  },
+  {
+    "revision": "1e0e1c69cfeb17c982b2840171e684bd",
+    "url": "/static/media/csec-proj.1e0e1c69.png"
+  },
+  {
+    "revision": "439de75bae8f62cbbf2aa5ff5b7486c8",
+    "url": "/static/media/me.439de75b.png"
+  },
+  {
+    "revision": "a2e41c6d759f272a01a5a1c5175b77f9",
+    "url": "/static/media/resume.a2e41c6d.pdf"
+  },
+  {
+    "revision": "e893f7f78381ae3d090f313d0fb4df7e",
+    "url": "/static/media/yellow.e893f7f7.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.78fd46b93624b3b10426b69f16a06561.js"
+  "/precache-manifest.70e588a51a7e71119e66a737a652008b.js"
 );
 
 self.addEventListener('message', (event) => {
